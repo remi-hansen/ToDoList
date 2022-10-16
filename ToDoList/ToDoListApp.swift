5684beba5a09f7fee57f985574f17fce844144ca
Remi Hansen <hansenrd@bc.edu>
2022-10-16 14:28:15 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Remi Pacifico Hansen on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
