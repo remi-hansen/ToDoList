1b0f603339358c98b217df24dfe25374d7daaf85
Remi Hansen <hansenrd@bc.edu>
2022-12-10 12:31:04 -0500

    forgot to push to remote

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,21 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Remi Pacifico Hansen on 10/17/22.
+//
+
+import Foundation
+
+struct ToDo: Identifiable, Codable {
+    var id: String?
+    
+    var item = ""
+    var reminderIsOn = false
+    var dueDate = Date.now + (60*60*24)
+    var notes = ""
+    var isCompleted = false
+}
+
+
+
---
+++ ToDoList/ToDoListView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Remi Pacifico Hansen on 10/16/22.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    @State private var sheetIsPresented = false
+    @EnvironmentObject var toDosVM: ToDosViewModel
+    
+    
+    var body: some View {
+        NavigationStack{
+            
+            List{
+                ForEach(toDosVM.toDos) { toDo in
+                    HStack{
+                        Image(systemName: toDo.isCompleted ? "checkmark.rectangle": "rectangle")
+                            .onTapGesture {
+                                toDosVM.toggleCompleted(toDo: toDo)
+                            }
+                        NavigationLink {
+                            DetailView(toDo: toDo)
+                        } label: {
+                            Text(toDo.item)
+                        }
+                        .font(.title2)
+                    }
+                }
+                //ShortHand calls to .onDelete and .onMove here
+                .onDelete(perform: toDosVM.deleteToDo)
+                .onMove(perform: toDosVM.moveToDo)
+                //Traditional calls are below
+//                .onDelete { indexSet in
+//                    toDosVM.delete(indexSet: indexSet)
+//                }
+//                .onMove { fromOffsets, toOffset in
+//                    toDosVM.move(fromOffsets: fromOffsets, toOffset: toOffset)
+//                }
+            }
+            .navigationTitle("To Do List")
+            .navigationBarTitleDisplayMode(.automatic)
+            .listStyle(.plain)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        sheetIsPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .sheet(isPresented: $sheetIsPresented) {
+                NavigationStack{
+                    DetailView(toDo: ToDo())
+                }
+            }
+//            .fullScreenCover(isPresented: $sheetIsPresented) {
+//                DetailView(passedValue: "")
+//            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListView()
+            .environmentObject(ToDosViewModel())
+    }
+}
+
